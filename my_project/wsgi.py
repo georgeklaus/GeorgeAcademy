13844634feca216f1b3662336aa535b8f2631e93
georgeklaus <164487115+georgeklaus@users.noreply.github.com>
2025-06-16 14:10:06 +0300

    I am fixing the static file rendering for vercel deployment

--- my_project/wsgi.py
+++ my_project/wsgi.py
@@ -16,11 +16,12 @@ from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
 
 application = get_wsgi_application()
-if settings.DEBUG:
-    application = WhiteNoise(application, root=settings.STATIC_ROOT)
-else:
-    application = WhiteNoise(application, root=settings.STATIC_ROOT)
-    application.add_files(settings.STATIC_ROOT, prefix='static/')
+if not settings.DEBUG:
+    application = WhiteNoise(
+        application,
+        root=settings.STATIC_ROOT,
+        prefix=settings.STATIC_URL
+    )
 application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
 
 app = application  # For Vercel
