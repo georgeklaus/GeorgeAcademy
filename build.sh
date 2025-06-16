cfea7d7ecd98732cd7bb216fbed81e0f86a3b93f
georgeklaus <164487115+georgeklaus@users.noreply.github.com>
2025-06-16 11:37:39 +0300

    I am fixing the static file rendering for vercel deployment

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-apt-get update && apt-get install -y libpq-dev
 
 # Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 # Run database migrations
-python3 manage.py migrate
+python manage.py migrate
