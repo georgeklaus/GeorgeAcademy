58f3587596553f2673f40784727cf37a29c728d9
georgeklaus <164487115+georgeklaus@users.noreply.github.com>
2025-06-17 16:23:15 +0300

    fixing the path configuration files

--- my_project/settings.py
+++ my_project/settings.py
@@ -92,7 +92,7 @@ ROOT_URLCONF = 'my_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
